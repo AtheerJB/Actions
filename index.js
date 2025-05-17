50cfe9500788ac7a4666019ba4a06f7c9086dc54
Atheer Alshahrani <143182728+AtheerJB@users.noreply.github.com>
2025-05-17 04:44:01 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function checkLogin(email, password, users) {
-    if (users[email] && users[email].password === password) {
-        return "Login successful";
-    } else {
-        return "Invalid email or password";
-    }
-}
-
-module.exports = { checkLogin };
